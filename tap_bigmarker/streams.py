6173dbb29b11dea084b55856d79ba9bf97316612
Lucas Crespo <v-lucas@pulumi.com>
2024-06-04 16:16:10 -0300

    missing streams added, all schemas updated, incremental mode added to conferences

--- tap_bigmarker/streams.py
+++ tap_bigmarker/streams.py
@@ -1,7 +1,10 @@
 """Stream type classes for tap-mindstamp."""
 
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, Optional, Iterable, Dict
+from datetime import datetime, timedelta, timezone
+import logging
+
 
 from tap_bigmarker.client import BigMarkerStream
 
@@ -14,7 +17,6 @@ class ChannelsStream(BigMarkerStream):
     records_jsonpath = "$.channels[*]"
     primary_keys = ["channel_id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "channels.json"
     has_pagination = False
 
@@ -29,7 +31,6 @@ class ChannelsSubscribersStream(BigMarkerStream):
     records_jsonpath = "$.subscribers[*]"
     primary_keys = ["bmid"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "channels_subscribers.json"
     parent_stream_type = ChannelsStream
     ignore_parent_replication_keys = True
@@ -42,7 +43,6 @@ class ChannelsAdminsStream(BigMarkerStream):
     records_jsonpath = "$[*]"
     primary_keys = ["admin_id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "channels_admins.json"
     parent_stream_type = ChannelsStream
     ignore_parent_replication_keys = True
@@ -55,24 +55,52 @@ class ConferencesStream(BigMarkerStream):
     rest_method = "POST"
     records_jsonpath = "$.conferences[*]"
     primary_keys = ["id"]
-    replication_method = "FULL_TABLE"
-    replication_keys = []
+    replication_method = "INCREMENTAL"
+    replication_key = "id"
     schema_filepath = SCHEMAS_DIR / "conferences.json"
 
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         """Return a context dictionary for child streams."""
         return {"conference_id": record["id"]}
     
-    def prepare_request_payload(
+    
+    def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Optional[dict]:
-        if next_page_token:
-            return { 
-                self.page_key: next_page_token,
-                "per_page": self.per_page
-            }
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+
+        state = self.get_context_state(context)
+        last_date = state.get('last_date', None)
+        last_date = datetime.fromtimestamp(state.get('last_date', 0),timezone.utc).replace(hour=0,minute=0,second=0,microsecond=0)
         
-        return None
+        if last_date > datetime(1970,1,1,tzinfo=timezone.utc):
+            last_date = last_date - timedelta(days=1)
+        
+        params: dict = {}
+        if next_page_token:
+                params[self.page_key] = next_page_token
+        if self.per_page:
+            params["per_page"] = self.per_page
+        params['start_time'] = int(last_date.timestamp())
+        
+
+        return params
+    
+    def get_records(self, context: Optional[dict]) -> Iterable[Dict[str, Any]]:
+        state = self.get_context_state(context)
+        logging.info(f"State: {state}")
+        for record in self.request_records(context):
+            transformed_record = self.post_process(record, context)
+            if transformed_record is None:
+                # Record filtered out during post_process()
+                continue
+            yield transformed_record
+
+        current_timestamp = datetime.now(timezone.utc).timestamp()    
+        state['last_date'] = current_timestamp
+
+            
+    
 
 class ConferencesHandoutsStream(BigMarkerStream):
     name = "conferences_handouts"
@@ -80,7 +108,6 @@ class ConferencesHandoutsStream(BigMarkerStream):
     records_jsonpath = "$[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_handouts.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
@@ -92,7 +119,6 @@ class ConferencesSurveysStream(BigMarkerStream):
     records_jsonpath = "$.survey[*]"
     primary_keys = ["question_title", "answer_type"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_surveys.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
@@ -104,7 +130,6 @@ class ConferencesPresentersStream(BigMarkerStream):
     records_jsonpath = "$.presenters[*]"
     primary_keys = ["presenter_id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_presenters.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
@@ -117,7 +142,6 @@ class ConferencesAttendeesStream(BigMarkerStream):
     records_jsonpath = "$.attendees[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_attendees.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
@@ -128,8 +152,18 @@ class ConferencesRegistrantsStream(BigMarkerStream):
     records_jsonpath = "$.registrations[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_registrants.json"
+    parent_stream_type = ConferencesStream
+    ignore_parent_replication_keys = True
+
+class ConferencesRegistrationsLiveStream(BigMarkerStream):
+    name = "conferences_registrations_live"
+    path = "/reporting/conferences/registrations/{conference_id}"
+    page_key = "current_page"
+    records_jsonpath = "$.registrations[*]"
+    primary_keys = ["id"]
+    replication_method = "FULL_TABLE"
+    schema_filepath = SCHEMAS_DIR / "conferences_registrants_live.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
 
@@ -140,10 +174,10 @@ class ConferencesAttendeesLiveStream(BigMarkerStream):
     records_jsonpath = "$.attendees[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_attendees_live.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
+
 
 class ConferencesRegistrationsNoShowsStream(BigMarkerStream):
     name = "conferences_registrations_no_shows"
@@ -152,10 +186,22 @@ class ConferencesRegistrationsNoShowsStream(BigMarkerStream):
     records_jsonpath = "$.registrations[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_registrations_no_shows.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
+
+
+class ConferencesRegistrationsQaStream(BigMarkerStream):
+    name = "conferences_registrations_qa"
+    path = "/reporting/conferences/q_and_a_transcript/{conference_id}"
+    page_key = "current_page"
+    records_jsonpath = "$.q_and_a[*]"
+    primary_keys = ["bmid"]
+    replication_method = "FULL_TABLE"
+    schema_filepath = SCHEMAS_DIR / "conferences_registrations_qa.json"
+    parent_stream_type = ConferencesStream
+    ignore_parent_replication_keys = True
+
 
 class ConferencesAttendeesOnDemandStream(BigMarkerStream):
     name = "conferences_attendees_on_demand"
@@ -164,7 +210,6 @@ class ConferencesAttendeesOnDemandStream(BigMarkerStream):
     records_jsonpath = "$.attendees[*]"
     primary_keys = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     schema_filepath = SCHEMAS_DIR / "conferences_attendees_on_demand.json"
     parent_stream_type = ConferencesStream
     ignore_parent_replication_keys = True
